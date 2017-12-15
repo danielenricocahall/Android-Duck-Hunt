2fc5f15c410eb16ef8c4b3578c6120900d4e943d
danielcahall <danielenricocahall@gmail.com>
2017-12-14 22:13:42 -0500

    Added dog animation

--- app/src/main/java/com/example/danie/ppd_final_project/Dog.java
+++ app/src/main/java/com/example/danie/ppd_final_project/Dog.java
@@ -21,12 +21,16 @@ public class Dog extends GameObject {
     Bitmap[] sprites;
     int frame = 0;
     DynamicPhysicsComponent physicsComponent;
+    float jumpingTime;
+    boolean barked;
 
 
     public Dog(DynamicPhysicsComponent physicsComponent)
     {
         this.physicsComponent = physicsComponent;
         readyToJump = false;
+        jumpingTime = 0.0f;
+        barked = false;
         sprites = new Bitmap[9];
         populateSprites(GameEngine.context);
         current_sprite = sprites[0];
@@ -40,6 +44,7 @@ public class Dog extends GameObject {
         position = new Vector2D(0.0f, -0.216667f);
         paint = new Paint();
         layer = GameConstants.FOREGROUND;
+
     }
 
     public void init()
@@ -62,19 +67,34 @@ public class Dog extends GameObject {
         else
         {
 
-            GameSoundHandler.stopLongSound();
-            GameSoundHandler.playLongSound(GameConstants.DOG_BARKING_SOUND);
-            if(current_sprite == sprites[5]) {
-                current_sprite = sprites[6];
-                this.physicsComponent.forward.y = -30.0f;
-            }
-            else
-            {
-                current_sprite = sprites[7];
-                this.physicsComponent.forward.y = 0.0f;
-                this.destroy = true;
+            if(!barked) {
+                GameSoundHandler.stopLongSound();
+                GameSoundHandler.playLongSound(GameConstants.DOG_BARKING_SOUND);
+                barked = true;
+                try
+                {
+                    Thread.sleep(300);
+                }
+                catch (InterruptedException e)
+                {
 
+                }
+                return;
             }
+            jumpingTime+=GameEngine.DELTA_TIME;
+            if(jumpingTime < 1.0f) {
+                current_sprite = sprites[6];
+                this.physicsComponent.forward.y = -GameEngine.SCREEN_HEIGHT*0.007f;
+            }
+            else if(jumpingTime > 1.0f && this.position.y > -0.216667f) {
+                current_sprite = sprites[7];
+                this.physicsComponent.forward.y = GameEngine.SCREEN_HEIGHT*0.007f;
+            }
+            else {
+                this.destroy = true;
+            }
+
+
         }
         canvas.drawBitmap(current_sprite, Camera.worldXToScreenX(position.x), Camera.worldYToScreenY(position.y), paint);
     }
